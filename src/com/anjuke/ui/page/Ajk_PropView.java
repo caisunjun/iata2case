56a81cb9da9cd311ac3dce76450cd878a6dc4402
ccyang <ccyang@anjuke.com>
2012-08-08 15:33:15 +0800

    add locater

--- src/com/anjuke/ui/page/Ajk_PropView.java
+++ src/com/anjuke/ui/page/Ajk_PropView.java
@@ -1,11 +1,47 @@
 package com.anjuke.ui.page;
 
 /**
- * 安居客 - 二手房详细页 http://shanghai.anjuke.com/prop/view/**
+ * 安居客 - 二手房房源单页 http://shanghai.anjuke.com/prop/view/**
  * 
  * @author grayhu
+ * @updateAuthor ccyang 
+ * Last update time 2012-8-7 16:00
  * */
 
 public class Ajk_PropView {
 
+	/* 房源信息 */
+	
+	/** 二手房单页 - 房源标题 */
+	public static final String HOUSETITLE = "className^propInfoTitle";
+	/** 二手房单页 - 总价 */
+	public static final String RICE = "//ul/li[1]/em";
+	/** 二手房单页 - 房型:X室X厅X卫*/
+	public static final String HOUSETYPE = "//ul[@class='prop_basic']/li[2]";
+	/** 二手房单页 - 面积 */
+	public static final String AREA = "//ul[@class='prop_basic']/li[3]";
+	/** 二手房单页 - 楼层：XX/YY层 */
+	public static final String FLOOR = "//ul[@class='prop_basic']/li[4]";
+	/** 二手房单页 - 单价 */
+	public static final String UNITPRICE = "//ul/li[7]/div[1]";
+	/** 二手房单页 - 朝向 */
+	public static final String CHAOXIANG = "//ul/li[7]/div[2]";
+	/** 二手房单页 - 装修 */
+	public static final String ZHUANGXIU = "//ul/li[7]/div[3]";
+	/** 二手房单页 - 建造年代 */
+	public static final String HOUSEAGE = "//ul/li[7]/div[4]";
+	/** 二手房单页 - 小区名 */
+	public static final String COMMNAME = "//a[@id='text_for_school_1']";
+	/** 二手房单页 - 房源编号 */
+	public static final String PROPID = "id^propID";
+	
+	/* 房源描述 */
+	
+	/** 二手房单页 - 房源描述 */
+	public static final String HOUSEDES = "//div[@class='propDescValue_p']/p";
+	
+	/* 近期评价 */
+	/* 地图 */
+	/* 小区简介 */
+	/* 等等 */
 }
--- src/com/anjuke/ui/page/Broker_PropertynewSaleStep.java
+++ src/com/anjuke/ui/page/Broker_PropertynewSaleStep.java
@@ -5,11 +5,67 @@ package com.anjuke.ui.page;
  * http://my.anjuke.com/user/broker/propertynew/sale/step1/W0QQoooflagZ1
  * 
  * @author grayhu
+ * @updateAuthor ccyang 
+ * Last update time 2012-8-7 14:20
  * */
 
 public class Broker_PropertynewSaleStep {
-
+	
+	/** 发房第一步*/
+	
 	/* 发布出售 - 请输入小区名 */
 	public static final String SALE_XIAOQU = "//input[@id='txtCommunity']";
+	/* 发布出售 - 下拉选择小区名 */
+	public static final String SALE_XIALAXIAOQU = "//div[@id='autocommunity']/ul[2]/li[2]";
+	/* 发布出售 - 房源价格 */
+	public static final String SALE_PRICE = "//input[@id='txtProPrice']";
+	/* 发布出售 - 产证面积 */
+	public static final String SALE_AREA = "//input[@id='txtAreaNum']";
+	/* 发布出售 - 房型-X室*/
+	public static final String SALE_ROOM = "//input[@id='txtRoomNum']";
+	/* 发布出售 - 房型-X厅*/
+	public static final String SALE_HALL = "//input[@id='txtHallNum']";
+	/* 发布出售 - 房型-X卫 */
+	public static final String SALE_TOILET = "//input[@id='txtToiletNum']";
+	/* 发布出售 - 当前楼层*/
+	public static final String SALE_LOUCENG = "//input[@id='txtProFloor']";
+	/* 发布出售 - 共几层 */
+	public static final String SALE_ZONGLOUCENG = "//input[@id='txtFloorNum']";
+	/* 发布出售 - 房屋类型  for select方法 后接选项名 */
+	public static final String SALE_LEIXING = "id^radUseType";
+	/* 发布出售 - 装修类型 for select方法 后接选项名*/
+	public static final String SALE_ZHUANGXIU = "id^radFitment";
+	/* 发布出售 - 朝向 */
+	public static final String SALE_CHAOXIANG = "id^radHouseOri";
+	/* 发布出售 - 建造年代*/
+	public static final String SALE_HOUSEAGE = "//input[@id='txtHouseAge']";
+	/* 发布出售 - 房源标题 */
+	public static final String SALE_HOUSETITLE = "//input[@id='txtProName']";
+	/* 发布出售 - 房源描述 */
+	public static final String SALE_HOUSEDES = "//td[@id='cke_contents_txtExplain']/iframe";
+	/* 发布出售 - 推荐该房源（不点才是推荐） */
+	public static final String SALE_RECOMMAND = "name^radSaleType";
+	/* 发布出售 - 去上传照片按钮 */
+	public static final String SALE_NEXTSTEP = "//input[@id='btnNext']";
+	/* 发布出售 - 立即发布按钮 */
+	public static final String SALE_FIRSTSUBMIT = "//input[@id='btnSubmit']";
+	
+	/** 发房第二步*/
+	
+	/* 发布出售 - 上传室内图-单张上传 */
+	public static final String SALE_SHINEISINGLE = "//input[@id='single_upload_apf_id_6']";
+	/* 发布出售 - 上传房型图-单张上传 */
+	public static final String SALE_FANGXINGSINGLE = "//input[@id='single_upload_apf_id_7']";
+	/* 发布出售 - 上传小区图-单张上传 */
+	public static final String SALE_XIAOQUSINGLE = "//input[@id='single_upload_apf_id_8']";
+	/* 发布出售 - 发布房源按钮 */
+	public static final String SALE_SECONDSUBMIT = "//input[@id='submitup']";
+	
+	/** 发房成功*/
+	
+	/* 发布出售 - 房源发布、编辑成功的提示语 */
+	public static final String SALE_SUCCESSMESSAGE = "//div[@class='p-popresult succeed']/h2";
+	/* 发布出售 - 查看房源单页 */
+	public static final String SALE_PROPLINK = "//div[@class='p-popresult succeed']/p/a[1]";
 
 }
