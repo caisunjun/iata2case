85b0b6f555503ca867266617017c5377d8c9418a
fjzhang <fjzhang@anjuke.com>
2012-08-08 16:24:09 +0800

    增加安居客 经纪人修改资料脚本

---
+++ src/com/anjuke/ui/page/Broker_info.java
@@ -0,0 +1,91 @@
+package com.anjuke.ui.page;
+
+/**
+* @Todo 经纪人首页
+* @author fjzhang
+* @since 2012-8-7
+* @file Ajk_BrokerIndex.java
+*  http://my.anjuke.com/user/modify/brokerinfo
+*/
+
+public class Broker_info {
+
+    /**
+     * 更新个人信息 链接
+     */
+    public static final String updateinfo = "html/body/div[2]/div[3]/div[2]/dl/dd[5]/a";
+
+    /**
+     *修改电子邮箱
+     */
+    public static final String updateemail = "id^wj_useemail_m";
+
+    /**
+     * 旧的邮件地址
+     */
+    public static final String oldemail = "//div[@class='boxer']/dl[1]/dd";
+    /**
+     * 新的邮件地址
+     */
+    public static final String newemail = "id^email";
+
+    /**
+     * 更改邮件 登录密码
+     */
+    public static final String password = "name^password";
+
+    /**
+     * 更改邮件提交按钮
+     */
+    public static final String btn = "html/body/div[2]/div[1]/div/form/div/dl[4]/dd/a";
+
+    /**
+     * 更改邮件成功提示
+     */
+    public static final String oktext = "id^tpok7";
+
+    /**
+     * 更改工作区域链接
+     */
+    public static final String UPDATEAREALINK = "id^wj_areaname_m";
+
+    /**
+     * 经纪人修改资料 手机号码
+     */
+    public static final String MOBILE = "id^usermobile";
+
+    /**
+     * 经济人修改资料 区域
+     */
+    public static final String AREA = "id^areaid";
+  /**
+   * 经济人修改资料 区域
+   */
+    public static final String BLOCK = "id^blockid";
+
+    /**
+     * 经纪人修改资料 公司
+     */
+    public static final String COMPANY  = "id^company_radio_1";
+    /**
+     * 经纪人修改资料  公司文本
+     */
+    public static final String COMPANYTEXT = "id^company";
+    /**
+     * 经纪人修改资料 独立经纪人
+     */
+    public static final String INDIVIDUAL = "id^company_radio_3";
+    /**
+     * 图片上传
+     */
+    public static final String IMAGEUPLOAD = "id^bns_card_file";
+    /**
+     * 确认按钮
+     */
+    public static final String SUBMIT = "id^btn_submit";
+    /**
+     * 提交成功 文本
+     */
+    public static final String INFOOKTEXT = "id^tpok32";
+}
+
