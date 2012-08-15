a1c6abb9c25418a93cefd82e07a029542964fd27
fjzhang <fjzhang@anjuke.com>
2012-08-15 17:40:27 +0800

    新增安居客 店铺管理脚本

---
+++ src/com/anjuke/ui/page/Broker_areas.java
@@ -0,0 +1,51 @@
+package com.anjuke.ui.page;
+
+
+/**
+* @Todo http://my.anjuke.com/user/broker/areas
+* @author fjzhang
+* @since 2012-8-15
+* @file Broker_areas.java
+*
+*/
+public class Broker_areas {
+
+    /**
+     * 最熟悉的区域板块列表
+     */
+    public static final String AREA1 = "//div[@id='selarea']/select";
+    public static final String  BLOCK1= "//div[@id='selblock']/select";
+    public static final String AREA2 = "//div[@id='selarea2']/select";
+    public static final String BLOCK2 = "//div[@id='selblock2']/select";
+
+    /**
+     * 最熟悉的小区1
+     */
+    public static final String COMM1 = "id^commName1";
+    /**
+     * 最熟悉的小区2
+     */
+    public static final String COMM2 = "id^commName2";
+    /**
+     * 最熟悉的小区3
+     */
+    public static final String COMM3 = "id^commName3";
+
+    /**
+     * 确认保存
+     */
+    public static final String OKENTER = "id^areasSubmit";
+
+    /**
+     * ajax的弹窗口，熟悉的小区
+     */
+    public static final String COMMDIV = "/html/body/div/div[2]";
+    public static final String IFRAME1= "id^ifrCommunityList1";
+    public static final String IFRAME2 = "id^ifrCommunityList2";
+    public static final String IFRAME3 = "id^ifrCommunityList3";
+    /**
+     * 保存成功
+     */
+    public static final String SAVEOK = "//*[@id='userbox']/ul";
+}
+
---
+++ src/com/anjuke/ui/page/Broker_shopview.java
@@ -0,0 +1,31 @@
+package com.anjuke.ui.page;
+
+/**
+* @Todo http://shanghai.anjuke.com/shop/view/147468
+* @author fjzhang
+* @since 2012-8-15
+* @file Broker_shopview.java
+*
+*/
+public class Broker_shopview {
+
+    /**
+     * 卖房宣言
+     */
+    public static final String SLOGAN = "//div[@id='content']/div[1]/div[1]/div[1]/div[1]/div[2]/h4";
+
+    /**
+     * 自我介绍
+     */
+    public static final String SELFINTRO = "//div[@id='content']/div[1]/div[2]/div[1]/div/div[1]/p";
+
+    /**
+     * 最熟悉的板块
+     */
+    public static final String KNOWNAREA = "//*[@id='content']/div[1]/div[1]/div[1]/div[1]/div[2]/dl/dd[2]";
+    /**
+     * 最熟悉的小区
+     */
+    public static final String KNOWNCOMM = "//*[@id='content']/div[1]/div[1]/div[1]/div[1]/div[2]/dl/dd[1]";
+}
+
