6c606926ace0d949ffdf71d9e19f226f7f102326
ccyang <ccyang@anjuke.com>
2012-09-06 15:55:02 +0800

    locater 搬迁

---
+++ src/com/anjuke/ui/page/Ajk_CommunityTrends.java
@@ -0,0 +1,19 @@
+package com.anjuke.ui.page;
+
+/**
+ * 安居客 - 小区单页 - 价格行情  http://shanghai.anjuke.com/community/trends/
+ * 
+ * @author ccyang
+ * */
+
+public class Ajk_CommunityTrends {
+	/** 小区价格行情页 -  主营门店图像（金牌） */
+	public static final String MasterStoreIMG = "//div[@class='imgGold']";
+	/** 小区价格行情页 -  主营门店logo */
+	public static final String MasterStore = "//div[@class='divMasterStoreLogo']";
+	/** 小区价格行情页 -  主营门店当前经纪人数 */
+	public static final String MasterStoreBrokerNum = "//*[@id='content']/div/div[4]/div[1]/div[2]/ul/li[1]/a";
+	/** 小区价格行情页 -  主营门店当前房源数 */
+	public static final String MasterStorePropNum = "//*[@id='content']/div/div[4]/div[1]/div[2]/ul/li[2]/a";
+	
+}
