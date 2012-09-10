faf412c6045fd9b5ed2cfb4451b9377ebd75c91d
fjzhang <fjzhang@anjuke.com>
2012-09-10 09:34:19 +0800

    暂停跑

--- src/com/anjuke/ui/testcase/AnjukeBrokerSaleEdit.java
+++ src/com/anjuke/ui/testcase/AnjukeBrokerSaleEdit.java
@@ -79,7 +79,7 @@ public class AnjukeBrokerSaleEdit {
 	}
 	
 	//(timeOut = 200000)
-	@Test
+	@Test (groups = {"unstable"})
 	public void editSale() throws InterruptedException{
 		driver.deleteAllCookies();
 		//Report.setTCNameLog("管理出售编辑-- AnjukeUpdateReleaseSale --Hendry_huang");
