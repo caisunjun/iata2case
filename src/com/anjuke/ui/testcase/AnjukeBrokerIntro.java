a55034d7a29963bb607bdc134fff31dff4b2abc5
chuzhaoqin <zhaoqinchu@anjuke.com>
2012-11-13 17:36:20 +0800

    管理店铺--个人介绍

--- src/com/anjuke/ui/testcase/AnjukeBrokerIntro.java
+++ src/com/anjuke/ui/testcase/AnjukeBrokerIntro.java
@@ -1,4 +1,6 @@
 package com.anjuke.ui.testcase;
+import java.util.Random;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -11,9 +13,9 @@ import com.anjuke.ui.page.*;
 import com.anjuke.ui.publicfunction.PublicProcess;
 
 /**
-* @Todo http://my.anjuke.com/user/broker/introduction
-* @author fjzhang
-* @since 2012-8-15
+* @Todo http://my.anjuke.com/user/broker/introduction & 验证个人介绍部分可否点击
+* @author fjzhang & chuzhaoqin
+* @since 2012-11-13
 * @file AnjukeBrokerIntro.java
 *
 */
@@ -23,6 +25,7 @@ public class AnjukeBrokerIntro {
     String username="test1";
     String passwd = "111111";
 
+
     @BeforeMethod
     public void setUp() {
         bs = FactoryBrowser.factoryBrowser();
@@ -31,10 +34,43 @@ public class AnjukeBrokerIntro {
     public void tearDown(){
         bs.quit();
         bs = null;
+        Report.seleniumReport("shanghai.anjuke.com", "房价评估页验证");
     }
     @Test
     public void testBrokerIntroduce(){
         PublicProcess.dologin(bs, username, passwd);
+        bs.get("http://my.anjuke.com/user/broker/profile");
+        //waitForPageToLoad(500);
+        bs.select(Ajk_ShopView.PROAndCity, "上海市");
+        bs.select(Ajk_ShopView.Region, "浦东新区");
+        bs.click(Ajk_ShopView.Region1, "公开");
+        //年龄不公开
+        bs.click(Ajk_ShopView.Age2, "不公开");
+        //生肖
+        bs.select(Ajk_ShopView.shenXiao, "羊");
+        bs.click(Ajk_ShopView.shenXiao2,"不公开");
+        //星座
+        bs.select(Ajk_ShopView.xingzuo, "射手座");
+        bs.click(Ajk_ShopView.xingzuo1, "公开");
+        
+        //生成书籍list
+        //waitForPageToLoad(500);
+	   	int n;
+        for(int i=0;i<5;i++){
+        	Random r = new Random();
+	 		n= r.nextInt(11)+1; 
+        	bs.click("//*[@id='hotTag_book']/tbody/tr[2]/td/a["+n+"]", "点击书籍");
+       int m;
+       for (int a=0;a<5;a++);
+            Random s = new Random();
+            m= s.nextInt(13)+1;
+        	bs.click("//*[@id='hotTag_food']/tbody/tr[2]/td/a["+m+"]", "选择美食");
+       int l;
+  	   for (int b=0;b<5;b++);
+  	        Random q = new Random();
+  	        l= q.nextInt(14)+1;
+  	        bs.click("//*[@id='hotTag_acti']/tbody/tr[2]/td/a["+l+"]", "选择娱乐活动");  
+        }	        ////bs.click(Ajk_ShopView.submit, "点击确认保存");
         bs.get(url);
         bs.findElement(Broker_profile.SELLHOUSE, "清空卖方宣言", 60).clear();
         String slogan = "安居客测试公告栏2011-1-20ss"+GetRandom.getrandom(1000);
@@ -55,5 +91,4 @@ public class AnjukeBrokerIntro {
         bs.assertContains(actualslogan, slogan);
         bs.assertContains(actualselfintro, selfintro);
     }
-}
-
+  }
