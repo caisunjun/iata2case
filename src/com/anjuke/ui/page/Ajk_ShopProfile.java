7c6953f95c43e4382285956de949a86909dfc6b1
jchen <jchen@anjuke.com>
2012-11-19 17:27:30 +0800

    新增经纪人列表页是否有生活照筛选功能检查

---
+++ src/com/anjuke/ui/page/Ajk_ShopProfile.java
@@ -0,0 +1,14 @@
+package com.anjuke.ui.page;
+
+/**
+ * 安居客 - 经纪人店铺自我介绍页 http://shanghai.anjuke.com/shop/profile/397427
+ * 
+ * @author jchen
+ * */
+
+public class Ajk_ShopProfile {
+	
+	/** 经纪人店铺自我介绍页 - 个人资料 - 生活照 */
+	public static final String photo = "//*[@class='live_pic']";
+	
+}
---
+++ src/com/anjuke/ui/testcase/AnjukeBrokerListPhoto.java
@@ -0,0 +1,66 @@
+package com.anjuke.ui.testcase;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import org.testng.annotations.Test;
+
+import com.anjukeinc.iata.ui.browser.Browser;
+import com.anjukeinc.iata.ui.browser.FactoryBrowser;
+import com.anjukeinc.iata.ui.report.Report;
+import com.anjuke.ui.page.*;
+
+/**该测试用例用来检查经纪人列表里，有生活照的经纪人筛选结果是否正确
+ * 包括：
+ * 经纪人店铺自我介绍生活照页是否有照片
+ * 
+ * @author jchen
+ */
+
+
+public class AnjukeBrokerListPhoto {
+    Browser bs = null;
+
+    @BeforeMethod
+    public void setUp() {
+        bs = FactoryBrowser.factoryBrowser();
+    }
+    @AfterMethod
+    public void tearDown(){
+        bs.quit();
+        bs = null;
+    }
+    @Test
+    public void testBrokerListPhoto(){
+    	bs.get("http://shanghai.anjuke.com");
+    	if(!bs.check("Ajk_HomePage.BROKERTAB"))
+    	{bs.refresh();}
+    	bs.click(Ajk_HomePage.BROKERTAB, "点击TAB里的经纪人链接");
+    	bs.click(Ajk_Tycoon.photo, "点击筛选是否有生活照复选框");
+    	//筛选是否有生活照
+    	
+    	bs.click(Ajk_Tycoon.headimg, "点击经纪人头像");
+    	bs.switchWindo(2);
+    	//切换到经纪人店铺
+    	
+    	bs.click(Ajk_ShopView.ProfileTab, "点击经纪人店铺自我介绍tab");
+    	//点击经纪人店铺自我介绍tab
+    	
+    	bs.click(Ajk_ShopProfile.photo, "点击经纪人店铺自我介绍页生活照链接");
+    	//点击经纪人店铺自我介绍页生活照链接
+    	
+    	if(bs.getElementCount("//*[@class='nophoto']/dl/dd") != 0)
+    	{
+    		System.out.println("这个经纪人无生活照");
+    		Report.writeHTMLLog("有生活照的经纪人筛选结果不正确", "筛选出的经纪人无生活照", Report.FAIL, bs.printScreen());
+
+    	}
+    	else
+    	{
+    		System.out.println("这个经纪人有生活照");
+    		Report.writeHTMLLog("有生活照的经纪人筛选结果正确", "筛选出的经纪人有生活照", Report.PASS, "");
+    	}
+    	//bs.closeAllwindow();
+    	bs.close();
+    }
+}
