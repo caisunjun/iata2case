07f16cc917794890ea7e85e906100603c238f5d0
jchen <jchen@anjuke.com>
2012-11-19 17:29:00 +0800

    新增经纪人列表页是否有回答问题筛选功能的检查

---
+++ src/com/anjuke/ui/page/Ajk_ShopQuestion.java
@@ -0,0 +1,18 @@
+package com.anjuke.ui.page;
+
+/**
+ * 安居客 - 经纪人店铺问答页 http://shanghai.anjuke.com/shop/question/913870
+ * 
+ * @author jchen
+ * */
+
+public class Ajk_ShopQuestion {
+	
+	/** 经纪人店铺问答页 - 目前还没有回答过问题 */
+	public static final String NO_QUESTION = "//*[@class='no_answer']";
+	
+	/** 经纪人店铺问答页 - 回答数 */
+	public static final String QUESTION = "//*[@class='borker_qainfo']/table/tbody/tr[2]/td[1]";
+
+	
+}
