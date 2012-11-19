89d1fa2f08271cf75e7c0b8e0ca950226d2b534e
jchen <jchen@anjuke.com>
2012-11-19 17:30:22 +0800

    新增小区单页中小区问答功能检查

---
+++ src/com/anjuke/ui/page/Ajk_AskSearch.java
@@ -0,0 +1,14 @@
+package com.anjuke.ui.page;
+
+/**
+ * 安居客 - 问答搜索结果页 http://shanghai.anjuke.com/ask/QE688BFE4BAA7E8AF81
+ * 
+ * @author jchen
+ * */
+
+public class Ajk_AskSearch {
+	
+	/** 问答搜索结果页 - 搜索输入框 */
+	public static final String QuestionInput = "//*[@id='keyword_apf_id_9']";
+	
+}
