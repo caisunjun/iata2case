65bd0f33e65c80c1cf38023d8ab6eb9f92c78c81
grayhu <grayhu@anjuke.com>
2012-12-07 17:48:47 +0800

    add pricing scripts
    Signed-off-by: grayhu <grayhu@anjuke.com>

--- src/com/anjuke/ui/page/Broker_PPC_PropmanageSale.java
+++ src/com/anjuke/ui/page/Broker_PPC_PropmanageSale.java
@@ -21,4 +21,12 @@ public class Broker_PPC_PropmanageSale {
 		return "//table[@id='datatable']/tbody/tr["+val+"]/td[6]/div/p[1]/a[1]";	
 	}
 	
+	/** 房源库 - 单条房源定价推广链接*/
+	public static final String PropPricing = "//*[@id='datatable']/tbody/tr[1]//p[2]/a[2]/span[2]";
+		
+	/** 房源库 - 单条房源竞价推广链接*/
+	public static final String PropBidding = "//*[@id='datatable']/tbody/tr[1]//p[2]/a[2]/span[2]";
+
+	
+	
 }
