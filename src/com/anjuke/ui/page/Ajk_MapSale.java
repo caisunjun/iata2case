f9fa682bff29077d877aa8301f907ee9e57da27e
ccyang <ccyang@anjuke.com>
2012-12-27 17:34:42 +0800

    新增房源单页周边信息检查脚本

---
+++ src/com/anjuke/ui/page/Ajk_MapSale.java
@@ -0,0 +1,25 @@
+package com.anjuke.ui.page;
+
+/**
+ * 安居客 - 地图找房 http://shanghai.anjuke.com/map/sale/
+ * 
+ * @author ccyang
+ * */
+
+public class Ajk_MapSale {
+	
+	/** 地图 - 左侧列表 - 区域房源时的提示*/
+	public static final String ListPropInfo = "//*[@id='prop-info-default']";
+	/** 地图 - 左侧列表 - 区域房源时的房源数*/
+	public static final String ListPropCount = "//*[@id='prop-found']";
+	/** 地图 - 左侧列表 - 房源载入中*/
+	public static final String ListPropLoading = "//*[@id='list-loading-layer']";
+	/** 地图 - 左侧列表 - 选中小区时的信息（全）*/
+	public static final String ListCommInfo = "//*[@id='prop-info-comm']";
+	/** 地图 - 左侧列表 - 选中小区时的小区名（全）*/
+	public static final String ListCommNameSelected = "//a[@id='list-comm-name']";
+	/** 地图 - 左侧列表 - 清除标记链接*/
+	public static final String ListClearComm = "//a[@id='clear-comm-btn']";
+	
+
+}
